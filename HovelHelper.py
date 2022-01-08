669517f577a023344c34caf9ea1ee3652a9a732d
IdioticLoser55 <idioticloser55@gmail.com>
2022-01-08 22:28:33 +0000

    got the basics of a bot working.

--- HovelHelper.py
+++ HovelHelper.py
@@ -1,8 +1,5 @@
 import discord
 
-#not sure if I need this.
-#print(discord.__version__)  # check to make sure at least once you're on the right version!
-
 # opens a file as readonly to obtain the token.
 token = open("token.txt", "r").read()
 
@@ -10,17 +7,33 @@ token = open("token.txt", "r").read()
 client = discord.Client()
 
 
+# function to find out external IP.
+async def whatsMyIP(message):
+    # need this to do what its actually supposed to do.
+    await message.channel.send(f"```Hello```")
+
+
+COMMANDS = {
+    "ip": whatsMyIP
+}
+
+
 @client.event  # event decorator/wrapper. More on decorators here: https://pythonprogramming.net/decorators-intermediate-python-tutorial/. Basically makes the following function as if it has been declared inside of the function event.
 async def on_ready():  # method expected by client. This runs once when connected
     print(f'We have logged in as {client.user}')  # notification of login.
 
-
 @client.event
 async def on_message(message):  # event that happens per any message.
+    if(message.content[0] == "?"):
+        # Need to check or have some try catch for empty ones.
+        # Also the ones that don't have a match.
+        await COMMANDS[message.content[1:len(message.content)]](message)
 
     # each message has a bunch of attributes. Here are a few.
     # check out more by print(dir(message)) for example.
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+
+
 
 
 client.run(token)  # recall my token was saved!
